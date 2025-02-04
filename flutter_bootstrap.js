247613ae4563438055f96cae8aacd301a7636e75
marcoxmediran <marcoxmediran@gmail.com>
2025-02-04 14:52:15 +0800

    completed parser error recovery

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3963042646"
+    serviceWorkerVersion: "3023442297"
   }
 });
